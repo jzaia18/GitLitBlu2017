fb8a7ba4f24f331c4d02f08318bf0c26dc7d1dc6
Khyber Sen <kkysen@gmail.com>
2017-12-02 14:07:17 -0500

    added Character

---
+++ src/main/java/gitlitblu/Displayable.java
@@ -0,0 +1,14 @@
+package gitlitblu;
+
+import processing.core.PApplet;
+
+/**
+ * 
+ * 
+ * @author Khyber Sen
+ */
+public interface Displayable {
+    
+    public void display(PApplet app);
+    
+}
--- src/main/java/gitlitblu/GitLitBlu.java
+++ src/main/java/gitlitblu/GitLitBlu.java
@@ -1,6 +1,7 @@
 package gitlitblu;
 
 import processing.core.PApplet;
+import processing.event.KeyEvent;
 
 /**
  * 
@@ -17,17 +18,30 @@ public class GitLitBlu extends PApplet {
     
     @Override
     public void settings() {
-        
+        size(720, 720);
     }
     
     @Override
     public void setup() {
-        character = new Character(this);
+        character = new Character(this, "Alphys.png", width * 0.5f, height * 0.5f);
     }
     
     @Override
     public void draw() {
-        
+        clear();
+        character.display(this);
+    }
+    
+    @Override
+    public void keyPressed(final KeyEvent event) {
+        super.keyPressed(event);
+        character.keyPressed(event);
+    }
+    
+    @Override
+    public void keyReleased(final KeyEvent event) {
+        super.keyReleased(event);
+        character.keyReleased(event);
     }
     
 }
