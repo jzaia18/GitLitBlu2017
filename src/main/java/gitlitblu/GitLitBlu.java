61cb08526255398cf381b13c949fa64e454b5538
Khyber Sen <kkysen@gmail.com>
2017-12-02 14:31:46 -0500

    got multiple keys to work at once

--- src/main/java/gitlitblu/GitLitBlu.java
+++ src/main/java/gitlitblu/GitLitBlu.java
@@ -10,8 +10,10 @@ import processing.event.KeyEvent;
  */
 public class GitLitBlu extends PApplet {
     
-    private Character character;
-    private Touhou t;
+    private final Keys keys = new Keys();
+    
+    private Sprite mainCharacter;
+    //    private Touhou t;
     
     public static void main(final String[] args) {
         PApplet.main(GitLitBlu.class.getName());
@@ -24,29 +26,30 @@ public class GitLitBlu extends PApplet {
     
     @Override
     public void setup() {
-    	t = new Touhou();
-    	t.populateAtTop(this, 10);
-        character = new Character(this, "Alphys.png", width * 0.5f, height * 0.5f);
+        mainCharacter = new Sprite(this, "Alphys.png", width * 0.5f, height * 0.5f);
+        //    	t = new Touhou();
+        //    	t.populateAtTop(this, 10);
     }
     
     @Override
     public void draw() {
-    	fill(150);
+        fill(150);
         clear();
-        t.display(this);
-        //        character.display(this);
+        mainCharacter.display(this);
+        //        t.display(this);
     }
     
     @Override
     public void keyPressed(final KeyEvent event) {
         super.keyPressed(event);
-        character.keyPressed(event);
+        keys.keyPressed(event);
+        mainCharacter.keyPressed(keys);
     }
     
     @Override
     public void keyReleased(final KeyEvent event) {
         super.keyReleased(event);
-        character.keyReleased(event);
+        keys.keyReleased(event);
     }
     
 }
