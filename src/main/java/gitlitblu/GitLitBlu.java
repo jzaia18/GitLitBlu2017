3118c6819e9df76d6b4840283045e4219ee54901
Khyber Sen <kkysen@gmail.com>
2017-12-02 11:54:32 -0500

    setup Processing

---
+++ src/main/java/gitlitblu/GitLitBlu.java
@@ -0,0 +1,16 @@
+package gitlitblu;
+
+import processing.core.PApplet;
+
+/**
+ * 
+ * 
+ * @author Khyber Sen
+ */
+public class GitLitBlu extends PApplet {
+    
+    public static void main(final String[] args) {
+        PApplet.main(GitLitBlu.class.getName());
+    }
+    
+}
