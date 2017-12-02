555d7bb927ca5cbc0fac7a23b456667034aff173
Khyber Sen <kkysen@gmail.com>
2017-12-02 13:17:06 -0500

    other class referencing PApplet

---
+++ src/main/java/gitlitblu/Character.java
@@ -0,0 +1,18 @@
+package gitlitblu;
+
+import processing.core.PApplet;
+
+/**
+ * 
+ * 
+ * @author Khyber Sen
+ */
+public class Character {
+    
+    private final PApplet app;
+    
+    public Character(final PApplet app) {
+        this.app = app;
+    }
+    
+}
--- src/main/java/gitlitblu/GitLitBlu.java
+++ src/main/java/gitlitblu/GitLitBlu.java
@@ -9,6 +9,8 @@ import processing.core.PApplet;
  */
 public class GitLitBlu extends PApplet {
     
+    private Character character;
+    
     public static void main(final String[] args) {
         PApplet.main(GitLitBlu.class.getName());
     }
@@ -20,7 +22,7 @@ public class GitLitBlu extends PApplet {
     
     @Override
     public void setup() {
-        
+        character = new Character(this);
     }
     
     @Override
